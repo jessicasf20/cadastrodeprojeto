ddc099f80983e068230865141cefa2485f963d29
Jéssica dos Santos Farias <jessica.f@academico.ifrn.edu.br>
2023-01-10 15:32:42 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/pi/projeto/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package ifrn.pi.projeto.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Chamou a método index");
+		return "home";
+	}
+
+}
