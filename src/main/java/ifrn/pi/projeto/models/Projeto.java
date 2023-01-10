004d9d1f6521ff2cf3b538cd292936d1c9819685
Jéssica dos Santos Farias <jessica.f@academico.ifrn.edu.br>
2023-01-10 16:07:17 -0300

    Criando a página de formulário para cadastro do projeto

--- src/main/java/ifrn/pi/projeto/models/Projeto.java
+++ src/main/java/ifrn/pi/projeto/models/Projeto.java
@@ -5,6 +5,37 @@ public class Projeto {
 	private String maricula;
 	private String curso;
 	private String email;
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMaricula() {
+		return maricula;
+	}
+
+	public void setMaricula(String maricula) {
+		this.maricula = maricula;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
