e65f0dcb812ab87d3fcac8c13d3129e7a3e0c0e5
Jéssica dos Santos Farias <jessica.f@academico.ifrn.edu.br>
2023-01-17 14:09:47 -0300

    configurando classe de modelo Projeto

--- src/main/java/ifrn/pi/projeto/models/Projeto.java
+++ src/main/java/ifrn/pi/projeto/models/Projeto.java
@@ -1,10 +1,29 @@
 package ifrn.pi.projeto.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Projeto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String maricula;
 	private String curso;
 	private String email;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
