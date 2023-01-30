b78db7e3b16deba4c7c7a1ebb14ce63e703d825c
Jonatan Custódio Rodrigues <jonatan.custodio@academico.ifrn.edu.br>
2023-01-30 15:17:54 -0300

    atualização

--- src/main/java/ifrn/pi/projeto/controllers/ProjetosController.java
+++ src/main/java/ifrn/pi/projeto/controllers/ProjetosController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import ifrn.pi.projeto.models.Docente;
 import ifrn.pi.projeto.models.Projeto;
 import ifrn.pi.projeto.repositories.DocenteRepository;
@@ -38,11 +37,11 @@ public class ProjetosController {
 	@PostMapping
 	public String salvar(@Valid Projeto projeto, BindingResult result) {
 
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return form(projeto);
-			
+
 		}
-		
+
 		System.out.println(projeto);
 		pr.save(projeto);
 		return "redirect:/projeto";
@@ -60,7 +59,7 @@ public class ProjetosController {
 	public ModelAndView detalhar(@PathVariable Long id, Docente docente) {
 		ModelAndView md = new ModelAndView();
 		Optional<Projeto> opt = pr.findById(id);
-		
+
 		if (opt.isEmpty()) {
 			md.setViewName("redirect:/projeto");
 			return md;
@@ -70,116 +69,116 @@ public class ProjetosController {
 		Projeto projeto = opt.get();
 
 		md.addObject("projeto", projeto);
-		
+
 		List<Docente> docentes = dr.findByProjeto(projeto);
 		md.addObject("docentes", docentes);
-		
+
 		return md;
 	}
-	
+
 	@PostMapping("/{idProjeto}")
 	public ModelAndView savarDocente(@PathVariable Long idProjeto, @Valid Docente docente, BindingResult result) {
 		ModelAndView md = new ModelAndView();
-		
-		if(result.hasErrors()) {
-			
+
+		if (result.hasErrors()) {
+
 			return detalhar(idProjeto, docente);
 		}
-		
+
 		System.out.println("Id do projeto: " + idProjeto);
 		System.out.println(docente);
-		
+
 		Optional<Projeto> opt = pr.findById(idProjeto);
-		if(opt.isEmpty()) {
+		if (opt.isEmpty()) {
 			md.setViewName("redirect:/projeto");
-			
+
 			return md;
-			
+
 		}
-		
+
 		Projeto projeto = opt.get();
 		docente.setProjeto(projeto);
-		
+
 		dr.save(docente);
 		md.setViewName("redirect:/projeto/{idProjeto}");
-		
+
 		return md;
 	}
-	
+
 	@GetMapping("/{id}/selecionar")
 	public ModelAndView selecionarProjeto(@PathVariable Long id) {
 		ModelAndView md = new ModelAndView();
 		Optional<Projeto> opt = pr.findById(id);
-		if(opt.isEmpty()) {
+		if (opt.isEmpty()) {
 			md.setViewName("redirect:/projeto");
 			return md;
 		}
 		Projeto projeto = opt.get();
 		md.setViewName("projetos/formProjeto");
 		md.addObject("projeto", projeto);
-		
+
 		return md;
 	}
+
 	@GetMapping("/{idProjeto}/docentes/{idDocente}/selecionar")
 	public ModelAndView selecionarDocente(@PathVariable Long idProjeto, @PathVariable Long idDocente) {
 		ModelAndView md = new ModelAndView();
-		
+
 		Optional<Projeto> optProjeto = pr.findById(idProjeto);
 		Optional<Docente> optDocente = dr.findById(idDocente);
-		
-		if(optProjeto.isEmpty() || optDocente.isEmpty()) {
+
+		if (optProjeto.isEmpty() || optDocente.isEmpty()) {
 			md.setViewName("redirect:/projetos");
 			return md;
 		}
-		
+
 		Projeto projeto = optProjeto.get();
 		Docente docente = optDocente.get();
-		
-		if(projeto.getId() != docente.getProjeto().getId()) {
+
+		if (projeto.getId() != docente.getProjeto().getId()) {
 			md.setViewName("redirect:/projetos");
 			return md;
-			
+
 		}
-		
+
 		md.setViewName("projetos/detalhes");
 		md.addObject("docente", docente);
 		md.addObject("projeto", projeto);
 		md.addObject("docentes", dr.findByProjeto(projeto));
-		
-		
+
 		return md;
 	}
-	
+
 	@GetMapping("/{id}/remover")
 	public String apagarProjeto(@PathVariable Long id) {
-		
+
 		Optional<Projeto> opt = pr.findById(id);
-		
-		if(!opt.isEmpty()) {
+
+		if (!opt.isEmpty()) {
 			Projeto projeto = opt.get();
-			
+
 			List<Docente> docentes = dr.findByProjeto(projeto);
-			
+
 			dr.deleteAll(docentes);
 			pr.delete(projeto);
-			
+
 		}
-		
+
 		return "redirect:/projeto";
 	}
-	
+
 	@GetMapping("/{idProjeto}/docentes/{idDocente}/remover")
 	public String apagarProjeto(@PathVariable Long idProjeto, @PathVariable Long idDocente) {
-		
+
 		Optional<Docente> opt = dr.findById(idDocente);
-		
-		if(!opt.isEmpty()) {
+
+		if (!opt.isEmpty()) {
 			Docente docente = opt.get();
 			dr.delete(docente);
-			
+
 		}
-		
+
 		return "redirect:/projeto/{idProjeto}";
 	}
-	
+
 }
